11a1f0cc1b4e1244a6e7a8cd1a25a73e84c98378
bikash-dev <bikash.dev.bk@gmail.com>
2023-02-20 03:23:52 +0600

    header done

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import Banner from './components/Banner/Banner';
 import Category from './components/Category/Category';
 import CustomerReview from './components/CustomerReviews/CustomerReview';
 import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
 import NewsTeller from './components/NewsTeller/NewsTeller';
 import Offer from './components/Offer/Offer';
 import PopularFood from './components/PopularFood/PopularFood';
 import PopularDrinks from './components/PopulerDrinks/PopularDrinks';
 import Reservation from './components/Reservation/Reservation';
 import ShowCase from './components/Showcase/ShowCase';
+import HeaderTop from './components/Header/HeaderTop/HeaderTop'
+import Navbar from './components/Header/Navbar/Navbar';
 
 function App() {
   return (
     <div className='Container'>
-      <Header></Header>
+      <HeaderTop></HeaderTop>
+      <Navbar></Navbar>
       <Banner></Banner>
       <Offer></Offer>
       <AboutUs></AboutUs>
