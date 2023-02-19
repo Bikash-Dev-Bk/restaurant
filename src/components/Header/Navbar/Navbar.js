ae57bc362f80caa4099b883f0f3cb7b1213b7952
bikash-dev <bikash.dev.bk@gmail.com>
2023-02-20 01:56:54 +0600

    navbar added

--- src/components/Header/Navbar/Navbar.js
+++ src/components/Header/Navbar/Navbar.js
@@ -1,20 +1,37 @@
-import React from 'react';
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
+import logo from "../../../assets/images/Logo 1.png";
+import { FaUser, FaCartArrowDown, FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
-    return (
-        <nav className='header'>
-            <div className='header-name'>
-                <h1>Mr. Cafe</h1>
-            </div>
-            <div >
-                <a href="/home">Home</a>
-                <a href="/about">About</a>
-                <a href="/services">Services</a>
-                <a href="/contacts">Contacts</a>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="Navbar">
+      <div>
+        <img src={logo} alt="" />
+      </div>
+      <div>
+        <a href="/home">Home</a>
+        <a href="/about">About Us</a>
+        <a href="/services">Coffee & Drinks</a>
+        <a href="/services">Food Menu</a>
+        <a href="/services">Reservation</a>
+        <a href="/contacts">Contacts Us</a>
+      </div>
+      <div className="Navbar-Right">
+        <div>
+          <FaSearch />
+        </div>
+        <div className="Navbar-icons">
+          <p>$ 00.0 / 0</p>
+          <FaCartArrowDown />
+        </div>
+        <div className="Navbar-icons">
+          <FaUser />
+          <p>Jacob</p>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
